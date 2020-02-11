6f8ac113d03f0be94a7c9b843441e145b1adabd2
Will Johnson <willjohnson@MacBook-Pro-2.local>
2020-02-11 16:18:30 -0700

    reset board with button

--- main.js
+++ main.js
@@ -3,34 +3,43 @@
 let player;
 
 let images = [
-    { name: 'vader', image: 'vader.jpg'},
-    { name: 'blueLs', image: 'blueLs.png'},
-    { name: 'yoda', image: 'yoda.png'},
-    { name: 'greenLs', image: 'greenLs.jpg'}
+    {image: 'vader.jpg'},
+    {image: 'blueLs.png'},
+    {image: 'yoda.png'},
+    {image: 'greenLs.gif'}
 ]
 
 let spin;
+let score;
+
 
 /*----- app's state (variables) -----*/
-let score = 1000;
+
 
 
 
 /*----- cached element references -----*/
 
 let slot1 = document.getElementById('s1');
+let s1 = slot1.innerHTML;
 let slot2 = document.getElementById('s2');
+let s2 = slot2.innerHTML;
 let slot3 = document.getElementById('s3');
-let totalScore = document.querySelector('.score-box');
+let s3 = slot3.innerHTML;
+let scoreEl = document.querySelector('.score-box');
 let message = document.querySelector('h3');
-let bet1 = document.querySelector('.bet1');
-let bet2 = document.querySelector('.bet2');
-let bet3 = document.querySelector('.bet3');
+let msg = message.innerHTML;
+let bet1 = document.getElementById('1');
+let bet2 = document.getElementById('2');
+let bet3 = document.getElementById('3');
 let spinner = document.querySelector('.spin');
+let reset = document.querySelector('.reset');
+let bets = document.querySelectorAll('.bet');
 /*----- event listeners -----*/
 
 bet1.addEventListener('click', function() {
-   console.log('button 1');
+  console.log('button 1')
+
 })
 bet2.addEventListener('click', function() {
     console.log('button 2');
@@ -39,14 +48,78 @@ bet2.addEventListener('click', function() {
     console.log('button 3');
  })
 
-spinner.addEventListener('click', function() {
+ 
+ spinner.addEventListener('click', function(e) {
+     score = 1000;
+     
+     spinSlot();
+     
+     winner();
+     
+     loser();
+     spinner.style.visibility = "hidden";
+     reset.style.visibility = "visible";
+     
+    })
 
-    message.innerHTML = 'Good Luck!';
+ reset.addEventListener('click', function() {
+     slot1.innerHTML = s1;
+     slot2.innerHTML = s2;
+     slot3.innerHTML = s3;
+     message.innerHTML = msg;
+     reset.style.visibility = "hidden";
+     spinner.style.visibility = "visible";
+
+ })
     
-    //function that spins each slot
     
-    //function that determines winner
-})
+    /*----- functions -----*/
+    
+    
+    
+    function spinSlot() {
+        
+        result1 = images[Math.floor(Math.random() * images.length)]
+        result2 = images[Math.floor(Math.random() * images.length)]
+        result3 = images[Math.floor(Math.random() * images.length)]
+        
+        const slot1Image = document.createElement('img')
+        slot1Image.src = "images/" + result1.image
+        
+        const slot2Image = document.createElement('img')
+        slot2Image.src = "images/" + result2.image
+        
+        const slot3Image = document.createElement('img')
+        slot3Image.src = "images/" + result3.image
+        
+        
+        
+        slot1.append(slot1Image);
+        slot2.append(slot2Image);
+        slot3.append(slot3Image);
+        
+        
+    };
+    
+    function winner() {
+        
+        if (result1 === result2 && result1 === result3) {
+            message.innerHTML = 'YOU WIN!';
+            scoreEl.innerHTML += 0;
+        } else {
+            message.innerHTML = 'You Lost... Try Again!'; 
+            scoreEl.innerHTML -= 100;
+        }
+    }
+    
+    function loser() {
+        if (scoreEl.innerHTML <= 0) {
+            spinner.parentNode.removeChild(spinner);
+        }
+    }
+    
+    
+    
+   
 
 
-/*----- functions -----*/
